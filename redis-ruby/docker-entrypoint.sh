1a5af82e5a836dbb1d5c4240f375b3169b2e67b0
itsmetommy <r1tommy@gmail.com>
2019-09-19 02:59:53 -0700

    updated to latest redis and removed ruby container

--- redis-ruby/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-gem install redis \
-  && wget http://download.redis.io/redis-stable/src/redis-trib.rb \
-  && echo "yes" | ruby redis-trib.rb create --replicas 1 \
-  10.0.0.2:7000 10.0.0.3:7001 10.0.0.4:7002 10.0.0.5:7003 10.0.0.6:7004 10.0.0.7:7005
